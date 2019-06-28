7d1ede2db0d2c712a5f7298a35bab6e9d0f2162c
Jack YANG <jingkangy91@gmail.com>
2019-06-28 16:18:38 +0800

    Set PATH for /usr/local/sbin, python and ruby.

--- init.fish
+++ init.fish
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 
-set -x PATH $HOME/.local/anaconda3/bin $PATH
+set -x PATH /usr/local/sbin $PATH
+set -x PATH /usr/local/anaconda3/bin $PATH
+set -x PATH /usr/local/opt/ruby/bin $PATH
+set -x PATH /usr/local/lib/ruby/gems/2.6.0/bin $PATH
